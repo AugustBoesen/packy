1e75b69a4d522c127e877d59cd78c3f4e3140ada
unknown <augustboesen@gmail.com>
2024-05-13 14:50:15 +0300

    updated styles and layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ let numero: number = 10;
 
 export default function Index() {
   return (
-    <main className='flex flex-row'>
-      {numero}
-      <button className='bg-emerald-500'>Start here</button>
-      <button className='btn'>TÄMÄ ON PUNAINEN</button>
+    <main>
+      <h1>Otsikko 1 h1</h1>
+      <h2>Otsikko 2 h2</h2>
+      <h3>Otsikko 3 h3</h3>
+      <p>Tämä on paragraph</p>
+      <button>Button</button>
     </main>
   );
 }
