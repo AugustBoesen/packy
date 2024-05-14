d0b5d95e638671e55d7741f88e9d65659b3a5b89
Veikka, Liljander <ad1542@student.jamk.fi>
2024-05-14 14:03:59 +0300

    100. header muutos

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,31 @@ import '../styles/header.css';
 export default function Header() {
   return (
     <header className="header">
-      <div className="header-text">PACKY</div>
+      <div className="header-logo">PACKY</div>
+      <ul className="nav-list">
+        <li>
+          <h3>
+            <a href="/about">About</a>
+          </h3>
+        </li>
+        <li>
+          <h3>
+            <a href="#">Lorum</a>
+          </h3>
+        </li>
+        <li>
+          <h3>
+            <a href="#">Ipsum</a>
+          </h3>
+        </li>
+        <li>
+          <h3>
+            <a href="https://bitcoin.org/en/choose-your-wallet" target="_blank">
+              Donate
+            </a>
+          </h3>
+        </li>
+      </ul>
     </header>
   );
 }
