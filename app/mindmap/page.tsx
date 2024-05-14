ae860207ce6bde7076025b6191ee136d382c7d79
ad1770 <ad1770@student.jamk.fi>
2024-05-14 15:37:40 +0300

    mindmap/page.tsx muokattu ja components/MinMap_Table.tsx muokattu

--- app/mindmap/page.tsx
+++ app/mindmap/page.tsx
@@ -1,3 +1,15 @@
+//importoidaan toiminnallisuus MinMap_Tablesta
+import Modal from '../../components/MinMap_Table';
+
+//määritellään ja exportataan funktio Mindmap()
 export default function Mindmap() {
-  return <main>Aktuaalinen sovellus tähän</main>;
+  //palauttaa reitille /mindmap toiminnallisuuden
+  return (
+    <main>
+      <>
+        Aktuaalinen sovellus tähän
+        <Modal />
+      </>
+    </main>
+  );
 }
