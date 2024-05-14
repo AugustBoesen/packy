791c5da9345433850fb0df0d9554a20ef3dcc524
unknown <augustboesen@gmail.com>
2024-05-14 14:40:12 +0300

    etusivun päivitys

--- app/page.tsx
+++ app/page.tsx
@@ -22,24 +22,28 @@ if (currentHour > 18 || currentHour < 5) {
 export default function Index() {
   return (
     <main className='flex-row'>
-      <div className='flex flex-col pr-6 md:max-w-[50vw] justify-center'>
+      <div className='flex flex-col pr-6 md:w-[50vw] justify-center'>
+        <h1 className='absolute font-bold blur-xl -z-10 top-32 opacity-100'>
+          {greeting}
+        </h1>
         <h1 className='font-bold bg-gradient-to-r from-emerald-400 via-green-200 to-indigo-200 inline-block text-transparent bg-clip-text'>
           {greeting}
         </h1>
+
         <p className='w-[26rem]'>
           Nowadays, grasping the scope of modern application development can be
           difficult at times.
         </p>
-        <p>
+        <p className='w-[32rem]'>
           Packy is a visual tool designed for developers of all skill levels to
           figure out the desired development tools for a project.
         </p>
-        <p className='mb-20'>
+        <p className='w-[36rem] mb-[5vh]'>
           It guides you through the basic steps for creating your desired
           application, taking into account both essential and quality-of-life
           tools.
         </p>
-        <h2 className='mb-20 text-center w-full italic bg-gradient-to-r from-emerald-400 via-green-200 to-indigo-200 inline-block text-transparent bg-clip-text'>
+        <h2 className='mb-[5vh] text-center w-full italic bg-gradient-to-r from-emerald-400 via-green-200 to-indigo-200 inline-block text-transparent bg-clip-text'>
           Shape your vision.
         </h2>
         <Link
@@ -51,6 +55,7 @@ export default function Index() {
         <Link className='indexbtn bg-teal-500 hover:bg-teal-200' href='/about'>
           Learn more about us
         </Link>
+        <div className='bg-emerald-200 w-1/2 blur-3xl h-96 absolute bottom-40 opacity-20' />
         <Link className='indexbtn bg-cyan-500 hover:bg-cyan-200' href='/donate'>
           Become a Patreon
         </Link>
