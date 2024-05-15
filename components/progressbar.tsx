6aff74f372a68bbf5d30efb58ba69825825c55a8
unknown <augustboesen@gmail.com>
2024-05-15 14:09:05 +0300

    progressbar päivitetty

--- components/progressbar.tsx
+++ components/progressbar.tsx
@@ -3,30 +3,31 @@ import React, { useState } from 'react';
 
 function ProgressBar() {
   // Tässä määritellään tila missä progres bar on ja se käytää useStatea ja se alkaa tilasta 1
-  const [currentStep, setCurrentStep] = useState(1);
+  const [currentStep, setCurrentStep] = useState(0);
   const maxSteps = 7; // Tämä muuttuja määrittää monta steppiä on yhteensä
+  const barSteps = maxSteps - 1;
 
   const handleNextStep = () => {
     // tänä fucktio lisää yhden SetCurrenStep muttujaan ja pitää huolen että se ei ylitä maximia
-    setCurrentStep((prevStep) => Math.min(prevStep + 1, maxSteps));
+    setCurrentStep((prevStep) => Math.min(prevStep + 1, barSteps));
   };
 
   const handlePreviousStep = () => {
-    // tämä tekee saman kun aiemman mutta miinustaa setCurrenSteppiä
-    setCurrentStep((prevStep) => Math.max(prevStep - 1, 1));
+    // tämä tekee saman kun aiemman mutta miinustaa setCurrentSteppiä
+    setCurrentStep((prevStep) => Math.max(prevStep - 1, 0));
   };
   // tekee ne ympyrät sen barin yläpuolella
   const circles = Array.from({ length: maxSteps }, (_, index) => index + 1);
 
   return (
     // pallojne ja baarin div
-    <div className="flex flex-col items-center w-4/5 ">
+    <div className=' w-full flex flex-col'>
       {/* tästä alkaa pallojen div */}
-      <div className=" flex mb-2 gap-20 ">
+      <div className=' flex -mb-6 bg-red-500 w-full justify-between'>
         {circles.map((step) => (
           <div
             key={step}
-            className={`w-8 h-8 flex items-center justify-center rounded-full border   ${
+            className={`z-50 w-8 h-8 flex items-center justify-center rounded-full border    ${
               step <= currentStep
                 ? 'bg-emerald-600 text-white'
                 : 'bg-white text-black'
@@ -37,21 +38,21 @@ function ProgressBar() {
         ))}
       </div>
       {/* tästä alkaa baarin div */}
-      <div className="w-4/5 bg-gray-200 rounded-full dark:bg-gray-700">
+      <div className=' bg-gray-200 rounded-full dark:bg-gray-700 w-full'>
         <div
-          className="bg-emerald-500    p-2 leading-none rounded-full"
-          style={{ width: `${(currentStep / maxSteps) * 100}%` }}
+          className='bg-emerald-500 py-2 rounded-full'
+          style={{ width: `${(currentStep / barSteps) * 100}%` }}
         ></div>
       </div>
-      <div className="flex justify-between mt-2">
+      <div className='flex justify-between mt-2'>
         <button
-          className="px-4 py-2 bg-emerald-500 text-white rounded hover:bg-emerald-600"
+          className='px-6 py-2 bg-emerald-500 text-white rounded hover:bg-emerald-600'
           onClick={handlePreviousStep}
         >
           Previous Step
         </button>
         <button
-          className="px-4 py-2 bg-emerald-500 text-white rounded hover:bg-emerald-600"
+          className='px-4 py-2 bg-emerald-500 text-white rounded hover:bg-emerald-600'
           onClick={handleNextStep}
         >
           Next Step
