191988f86e7b5b92241bcdb87768d026fbb643e5
unknown <augustboesen@gmail.com>
2024-05-15 16:25:32 +0300

    päivitys contenttiin

--- components/content.tsx
+++ components/content.tsx
@@ -2,6 +2,8 @@
 import Modal from './MinMap_Table';
 import { useState } from 'react';
 
+let chosenArray: any[] = [];
+
 export default function Content({ setCurrentContent }: any) {
   const [datafromMinMap, setDatafromMinMap] = useState([]);
   const handleDatafromMinMap = (newDatafromMinMap: any) => {
@@ -10,7 +12,22 @@ export default function Content({ setCurrentContent }: any) {
 
   const handleContentChange = (newContent: any) => {
     setCurrentContent(newContent);
+    if (newContent.id !== undefined) {
+      const existingObjectIndex = chosenArray.findIndex(
+        (item) => item.code === newContent.code
+      );
+
+      if (existingObjectIndex === -1) {
+        chosenArray.push(newContent);
+      } else {
+        // Update the existing object with the same id instead of pushing a new one
+        chosenArray[existingObjectIndex] = newContent;
+      }
+    }
+
+    console.log(chosenArray);
   };
+
   return (
     <div>
       <Modal handleDatafromMinMap={handleDatafromMinMap} />
