f143eec1c7ba00e26f720e22f0cecb7199b356c8
Veikka, Liljander <ad1542@student.jamk.fi>
2024-05-15 13:17:12 +0300

    headeriririririiiirir

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,8 @@
 import '../styles/header.css';
 
+// import { usePathname } from 'next/navigation';
+// import Link from 'next/link';
+
 export default function Header() {
   return (
     <header className="header">
@@ -7,19 +10,20 @@ export default function Header() {
         {' '}
         <a href="/">PACKY</a>
       </div>
+
       <ul className="nav-list">
         <li>
-          <h3>
+          <h3 className=" hover:text-emerald-500 ease-out hover:translate-y-1 transition-all rounded">
             <a href="/mindmap">Start</a>
           </h3>
         </li>
         <li>
-          <h3>
+          <h3 className="hover:text-emerald-500 ease-out hover:translate-y-1 transition-all rounded">
             <a href="/about">About</a>
           </h3>
         </li>
         <li>
-          <h3>
+          <h3 className="hover:text-yellow-300 ease-out hover:translate-y-1 transition-all rounded">
             <a href="https://bitcoin.org/en/choose-your-wallet" target="_blank">
               Donate
             </a>
