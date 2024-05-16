550c2ba38a538a2610c17a465ee6004fb73379b7
ad1770 <ad1770@student.jamk.fi>
2024-05-16 11:54:43 +0300

    Lisättiin Toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import '../styles/globals.css';
 import Footer from '../components/footer';
 import Header from '../components/header';
 import Image from 'next/image';
-
+import { Toaster } from 'react-hot-toast';
 const montserrat = Montserrat({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -23,11 +23,12 @@ export default function RootLayout({
         <Header />
         <Image
           src={'/mainbg.jpg'}
-          alt='mainbg'
+          alt="mainbg"
           width={1920}
           height={1080}
-          className='blur-3xl opacity-50 absolute -z-50 max-h-[90vh]'
+          className="blur-3xl opacity-50 absolute -z-50 max-h-[90vh]"
         />
+        <Toaster position="bottom-left" />
         {children}
         <Footer />
       </body>
