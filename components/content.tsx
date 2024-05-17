5bcfe146f85aa5d1a881ef7313f45475abc68b4c
Petri Paasila <ad1782@student.jamk.fi>
2024-05-17 09:45:39 +0300

    kommentoitu content.tsx

--- components/content.tsx
+++ components/content.tsx
@@ -1,3 +1,7 @@
+// Komponentti hallitsee sovelluksen näkymää (näyttää joka vaiheessa oikeat objektit),
+//tieto kulkee MinMap_Tablesta tähän komponenttiin, joka välittää tiedon mindmap
+//moduulissa olevaan page.tsxään
+
 'use client';
 import Modal from './MinMap_Table';
 import { useState } from 'react';
@@ -48,24 +52,24 @@ export default function Content({ setCurrentContent }: any) {
         handleDatafromMinMap={handleDatafromMinMap}
         mmfinishSignal={setfinishSignal}
       />
-      <div className='flex w-full justify-center text-center rounded-fullh-24'>
+      <div className="flex w-full justify-center text-center rounded-fullh-24">
         <button
-          className='rounded-full z-50 h-32 w-32 -mt-80 font-bold bg-teal-500 hover:scale-125 transition-all duration-500 border hover:border-dashed hover:border-slate-300 hover:bg-teal-200 hover:text-black'
+          className="rounded-full z-50 h-32 w-32 -mt-80 font-bold bg-teal-500 hover:scale-125 transition-all duration-500 border hover:border-dashed hover:border-slate-300 hover:bg-teal-200 hover:text-black"
           onClick={() => handleContentChange(datafromMinMap)}
         >
           Add to plan
         </button>
       </div>
       {finishSignal == true && (
-        <ul className='absolute h-[80vh] w-full bg-slate-800 top-[10vh] z-[60] px-6'>
-          <h3 className='pt-4'>Chosen tools:</h3>
+        <ul className="absolute h-[80vh] w-full bg-slate-800 top-[10vh] z-[60] px-6">
+          <h3 className="pt-4">Chosen tools:</h3>
           {chosenArray.map((item, index) => (
             <>
-              <div className='border-t flex flex-row mt-6'>
-                <li className='font-bold shadow-inner min-w-40' key={index}>
+              <div className="border-t flex flex-row mt-6">
+                <li className="font-bold shadow-inner min-w-40" key={index}>
                   {item.name}
                 </li>
-                <li className='shadow-inner' key={index}>
+                <li className="shadow-inner" key={index}>
                   {item.description}
                 </li>
               </div>
